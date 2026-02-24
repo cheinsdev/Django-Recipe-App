0aa00c87d9d7b05ea64ed72d2030a600c87eae74
Lo3z <carlosheins1@gmail.com>
2026-02-24 15:23:23 -0500

    Implementing login/logout functionality, create search view/template

--- src/recipe_project/urls.py
+++ src/recipe_project/urls.py
@@ -18,13 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from recipes.views import home
-
+from recipes.views import home, search
+from .views import login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('recipes/', include('recipes.urls'))
+    path('recipes/', include('recipes.urls')),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 def home(request):
   return render(request, 'recipes/recipes_home.html')
 
-class RecipeListView(ListView):
+@login_required
+def search(request):
+  return render(request, 'recipes/recipes_search.html')
+
+class RecipeListView(LoginRequiredMixin, ListView):
   model = Recipe
   template_name = 'recipes/recipes_main.html'
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
   model = Recipe
   template_name = 'recipes/recipes_detail.html'
