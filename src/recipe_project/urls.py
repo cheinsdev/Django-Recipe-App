e6f6bfe41896ca8b80e0ba93460312200bb3156c
Lo3z <carlosheins1@gmail.com>
2026-02-26 14:58:30 -0500

    troubleshooting image display

--- src/recipe_project/urls.py
+++ src/recipe_project/urls.py
@@ -29,5 +29,4 @@ urlpatterns = [
     path('logout/', logout_view, name='logout')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
